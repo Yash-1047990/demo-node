f00198ca330c50cf1fd487c177243b4186ede375
Yash-1047990 <113020766+Yash-1047990@users.noreply.github.com>
2023-10-25 14:59:34 +0200

    Made the create a channel

--- nodes/NasaPics/NasaPics.node.ts
+++ nodes/NasaPics/NasaPics.node.ts
@@ -1,14 +1,14 @@
 import { INodeType, INodeTypeDescription } from 'n8n-workflow';
 
 export class NasaPics implements INodeType {
-	description: INodeTypeDescription = {
+    description: INodeTypeDescription = {
         displayName: 'Discord+',
         name: 'NasaPics',
         icon: 'file:discord.svg',
         group: ['transform'],
         version: 1,
-        subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
-        description: 'Get data from NASAs API',
+        subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
+        description: 'Get data from NASA\'s API',
         defaults: {
             name: 'Discord+',
         },
@@ -27,165 +27,100 @@ export class NasaPics implements INodeType {
                 'Content-Type': 'application/json',
             },
         },
-		properties: [
-        {
-            displayName: 'Resource',
-            name: 'resource',
-            type: 'options',
-            noDataExpression: true,
-            options: [
-                {
-                    name: 'Astronomy Picture of the Day',
-                    value: 'astronomyPictureOfTheDay',
+        properties: [
+            {
+                displayName: 'Operation',
+                name: 'operation',
+                type: 'options',
+																default: 'create',
+                noDataExpression: true,
+                displayOptions: {
+                    show: {
+                        resource: ['channel'],
+                    },
                 },
-                {
-                    name: 'Mars Rover Photos',
-                    value: 'marsRoverPhotos',
+                options: [
+                    {
+                        name: 'Create',
+                        value: 'create',
+                        description: 'Create a channel',
+                        action: 'Create a channel',
+                    },
+                ]
+            },
+            /* -------------------------------------------------------------------------- */
+            /*                                 channel:create                             */
+            /* -------------------------------------------------------------------------- */
+            {
+                displayName: 'Team Name or ID',
+                name: 'teamId',
+                required: true,
+                type: 'options',
+                description: 'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>',
+                typeOptions: {
+                    loadOptionsMethod: 'getTeams',
                 },
-            ],
-            default: 'astronomyPictureOfTheDay',
-        },
-	{
-	displayName: 'Operation',
-	name: 'operation',
-	type: 'options',
-	noDataExpression: true,
-	displayOptions: {
-		show: {
-			resource: [
-				'astronomyPictureOfTheDay',
-			],
-		},
-	},
-	options: [
-		{
-			name: 'Get',
-			value: 'get',
-			action: 'Get the APOD',
-			description: 'Get the Astronomy Picture of the day',
-			routing: {
-				request: {
-					method: 'GET',
-					url: '/planetary/apod',
-				},
-			},
-		},
-	],
-	default: 'get',
-},
-{
-	displayName: 'Operation',
-	name: 'operation',
-	type: 'options',
-	noDataExpression: true,
-	displayOptions: {
-		show: {
-			resource: [
-				'marsRoverPhotos',
-			],
-		},
-	},
-	options: [
-		{
-			name: 'Get',
-			value: 'get',
-			action: 'Get Mars Rover photos',
-			description: 'Get photos from the Mars Rover',
-			routing: {
-				request: {
-					method: 'GET',
-				},
-			},
-		},
-	],
-	default: 'get',
-},
-{
-	displayName: 'Rover name',
-	description: 'Choose which Mars Rover to get a photo from',
-	required: true,
-	name: 'roverName',
-	type: 'options',
-	options: [
-		{name: 'Curiosity', value: 'curiosity'},
-		{name: 'Opportunity', value: 'opportunity'},
-		{name: 'Perseverance', value: 'perseverance'},
-		{name: 'Spirit', value: 'spirit'},
-	],
-	routing: {
-		request: {
-			url: '=/mars-photos/api/v1/rovers/{{$value}}/photos',
-		},
-	},
-	default: 'curiosity',
-	displayOptions: {
-		show: {
-			resource: [
-				'marsRoverPhotos',
-			],
-		},
-	},
-},
-{
-	displayName: 'Date',
-	description: 'Earth date',
-	required: true,
-	name: 'marsRoverDate',
-	type: 'dateTime',
-	default:'',
-	displayOptions: {
-		show: {
-			resource: [
-				'marsRoverPhotos',
-			],
-		},
-	},
-	routing: {
-		request: {
-			// You've already set up the URL. qs appends the value of the field as a query string
-			qs: {
-				earth_date: '={{ new Date($value).toISOString().substr(0,10) }}',
-			},
-		},
-	},
-},
-{
-	displayName: 'Additional Fields',
-	name: 'additionalFields',
-	type: 'collection',
-	default: {},
-	placeholder: 'Add Field',
-	displayOptions: {
-		show: {
-			resource: [
-				'astronomyPictureOfTheDay',
-			],
-			operation: [
-				'get',
-			],
-		},
-	},
-	options: [
-		{
-			displayName: 'Date',
-			name: 'apodDate',
-			type: 'dateTime',
-			default: '',
-			routing: {
-				request: {
-					// You've already set up the URL. qs appends the value of the field as a query string
-					qs: {
-						date: '={{ new Date($value).toISOString().substr(0,10) }}',
-					},
-				},
-			},
-		},
-	],									
-}
-
-
-
-]
-
-	};
+                displayOptions: {
+                    show: {
+                        operation: ['create'],
+                        resource: ['channel'],
+                    },
+                },
+                default: '',
+            },
+            {
+                displayName: 'Name',
+                name: 'name',
+                required: true,
+                type: 'string',
+                displayOptions: {
+                    show: {
+                        operation: ['create'],
+                        resource: ['channel'],
+                    },
+                },
+                default: '',
+                description: 'Channel name as it will appear to the user in Microsoft Teams',
+            },
+            {
+                displayName: 'Options',
+                name: 'options',
+                type: 'collection',
+                displayOptions: {
+                    show: {
+                        operation: ['create'],
+                        resource: ['channel'],
+                    },
+                },
+                default: {},
+                placeholder: 'Add Field',
+                options: [
+                    {
+                        displayName: 'Description',
+                        name: 'description',
+                        type: 'string',
+                        default: '',
+                        description: "Channel's description",
+                    },
+                    {
+                        displayName: 'Type',
+                        name: 'type',
+                        type: 'options',
+                        options: [
+                            {
+                                name: 'Private',
+                                value: 'private',
+                            },
+                            {
+                                name: 'Standard',
+                                value: 'standard',
+                            },
+                        ],
+                        default: 'standard',
+                        description: 'The type of the channel',
+                    },
+                ],
+            },
+        ],
+    };
 }
